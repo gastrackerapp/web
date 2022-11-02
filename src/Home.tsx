c0f5901b3c88d68024efb6a8b6e80cc3a4d9ce3e
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-11-02 09:21:28 +0100

    feat: :rocket: Working form

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import Form from "./components/Form";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 export default function Home() {
+  const getInitialState = () => {
+    const value = "Orange";
+    return value;
+  };
+
+  const [value, setValue] = useState(getInitialState);
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  let navigate = useNavigate();
+  const navigateHome = (value:string) => {
+    navigate('/stations?IDMUNICIPIO='+value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Home</p>
+        <div>
+          <select value={value} onChange={handleChange}>
+            <option value="Orange">Orange</option>
+            <option value="Radish">Radish</option>
+            <option value="Cherry">Cherry</option>
+          </select>
+          <p>{`You selected ${value}`}</p>
+        </div>
+        <button onClick={() => navigateHome(value)}>Home</button>
       </header>
     </div>
   );
