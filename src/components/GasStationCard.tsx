bddbf0ee10ecefd6cd9c4b314d2f380e97cfa8a9
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-11-28 23:06:46 +0100

    fix: :art: Fixed logo size

--- src/components/GasStationCard.tsx
+++ src/components/GasStationCard.tsx
@@ -1,8 +1,33 @@
 import "./GasStationCard.css";
-import logo from "./images/logo.png";
+import Bp from "./images/bp.png";
+import Cepsa from "./images/cepsa.png";
+import Galp from "./images/galp.png";
+import Petroil from "./images/petroil.png";
+import Petronor from "./images/petronor.png";
+import Repsol from "./images/repsol.png";
+import sinLogo from "./images/sinLogo.png";
+
 
 export default function GasStationCard(gasStation: any) {
   gasStation = gasStation.gasStation;
+  const getLogo = (Rótulo: String) => {
+    let logo = sinLogo;
+    if (Rótulo.search("BP") !== -1) {
+      logo = Bp;
+    }else if (Rótulo.search("CEPSA") !== -1) {
+      logo = Cepsa;
+    }else if (Rótulo.search("GALP") !== -1) {
+      logo = Galp;
+    }else if (Rótulo.search("PETROIL") !== -1) {
+      logo = Petroil;
+    }else if (Rótulo.search("PETRONOR") !== -1) {
+      logo = Petronor;
+    }else if (Rótulo.search("REPSOL") !== -1) {
+      logo = Repsol;
+    }
+    return logo;
+  }
+
   const direction: string =
     "https://www.google.com/maps/search/?api=1&query=" +
     gasStation.Latitud.replace(",", ".") +
@@ -10,7 +35,7 @@ export default function GasStationCard(gasStation: any) {
     gasStation.Longitud.replace(",", ".");
   return (
     <div className="GasStationCard">
-        <img className="GasStationLogo" src={logo}></img>
+        <img className="GasStationLogo" src={getLogo(gasStation.Rótulo)}></img>
       <div className="GasStationInfo">
         <div className="CardFirstRow">
           <p className="GasStationLabel">{gasStation.Rótulo} </p>
